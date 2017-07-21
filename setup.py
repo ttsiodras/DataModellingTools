39cc32cbf479c8fafd80afdc64a1f57d41a5fd7b
Maxime Perrotin <Maxime.Perrotin@esa.int>
2017-07-21 16:44:08 +0200

    Update version

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='dmt',
-    version="2.1.4",
+    version="2.1.5",
     packages=find_packages(),
     author='Thanassis Tsiodras',
     author_email='Thanassis.Tsiodras@esa.int',
