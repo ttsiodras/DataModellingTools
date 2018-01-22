98c85e52e728383cbe6a63f3ea74c4e534137c25
Maxime Perrotin <Maxime.Perrotin@esa.int>
2018-01-22 14:57:26 +0100

    Add debug output when asn1 fails

--- dmt/asn2aadlPlus.py
+++ dmt/asn2aadlPlus.py
@@ -93,7 +93,10 @@ def calculateForNativeAndASN1SCC(absASN1SCCpath, autosrc, names, inputFiles):
         for line in os.popen("%s -AdaUses %s" % (absASN1SCCpath, '" "'.join(inputASN1files))):
             g_AdaPackageNameOfType[line.split(':')[0]] = line.split(':')[1].rstrip()
     else:
-        mysystem("mono %s -wordSize 8 -c -uPER -o \"%s\" %s %s" % (absASN1SCCpath, autosrc, acn, '"' + '" "'.join(inputFiles) + '"'))
+        cmd = "mono %s -wordSize 8 -c -uPER -o \"%s\" %s %s" % (absASN1SCCpath, autosrc, acn, '"' + '" "'.join(inputFiles) + '"')
+        res = mysystem(cmd)
+        if res != 0 and configMT.debugParser:
+            print("This command failed: %s\n" % cmd)
         for line in os.popen('mono  %s -AdaUses "%s"' % (absASN1SCCpath, '" "'.join(inputASN1files))):
             g_AdaPackageNameOfType[line.split(':')[0]] = line.split(':')[1].rstrip()
 
