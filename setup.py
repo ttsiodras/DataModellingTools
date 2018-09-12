cb79ab60e4ab2ece44c8c21807102be29c2f4e7f
Thanassis Tsiodras <Thanassis.Tsiodras@esa.int>
2018-09-12 15:19:45 +0200

    Accuracy of datamodel.py used by Maxime's GUI.

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='dmt',
-    version="2.1.20",
+    version="2.1.21",
     packages=find_packages(),
     author='Thanassis Tsiodras',
     author_email='Thanassis.Tsiodras@esa.int',
