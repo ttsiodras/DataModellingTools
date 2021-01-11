cc7c0ba4f4eef2d89d76a55e1f336ae2e000ccc3
Maxime Perrotin <Maxime.Perrotin@esa.int>
2021-01-11 19:37:45 +0100

    Appease flake8

--- dmt/A_mappers/simulink_A_mapper.py
+++ dmt/A_mappers/simulink_A_mapper.py
@@ -213,7 +213,7 @@ def CreateDeclarationForType(nodeTypename: str, names: AST_Lookup, leafTypeDict:
             return
             # Ignore empty sequences in the A mapper, but do not raise an error, as empty sequences
             # may be used in other parts of the system.
-            #panic("Simulink_A_mapper: Simulink can't support empty Seq/Set/Choice! (%s)" % node.Location())  # pragma: no cover
+            # panic("Simulink_A_mapper: Simulink can't support empty Seq/Set/Choice! (%s)" % node.Location())  # pragma: no cover
         elemNo = 0
         if isinstance(node, AsnChoice):
             elemNo += 1
